716b63883a69a79696177fa49643e1888e7721ae
arvinarun <arvinarunroy00@gmail.com>
2026-07-19 15:58:09 +0530

    Learning Variable

---
+++ 01. Basics/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Arvin";
+        int age = 20;
+        double height = 1.7;
+        double weight = 57.5;
+        boolean learningjava = true;
+
+        System.out.println("---- Profile ----");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height(meters): " + height + "m");
+        System.out.println("Weight(kilograms): " + weight + "kg");
+        System.out.println("Learning Java?: " + learningjava);
+    }    
+}
