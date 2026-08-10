fa97a56a0fdc834797e0fc4cd51abed1ece4b253
arvinarun <arvinarunroy00@gmail.com>
2026-08-10 11:45:34 +0530

    Adding working log feature

--- Login System/LoginAnalyzer.java
+++ Login System/LoginAnalyzer.java
@@ -5,6 +5,9 @@ public class LoginAnalyzer {
         Scanner input = new Scanner (System.in);
 
         String[] usernames = {"Arvin", "Adam", "Bob", "Peter"};
+        String[] passwords = {"aeafhbk", "asdgs", "hdfssdh", "vsdfsd"};
+        String[] log = new String[10];
+        int loginCount = 0;
         System.out.println("---- Welcome ----");
         boolean welcomeLoop = true; 
         do {
@@ -12,20 +15,18 @@ public class LoginAnalyzer {
             String loginUsername = input.nextLine();
             int usernameIndex = usernameCheck(loginUsername, usernames);
             if (usernameIndex >= 0) {
-                welcomeLoop = false;
-                int passwordTrys = 3, successTrys = 0, failTrys = 0;
+                int passwordTrys = 3;
                 String login = "NULL";
                 do {
                     System.out.print("Enter password: ");
                      String loginPassword = input.nextLine();
-                     boolean passwordValid = passwordCheck(loginPassword, usernameIndex);
+                     boolean passwordValid = passwordCheck(loginPassword, usernameIndex, passwords);
                     if (passwordValid) {
-                        successTrys++;
                         login = "SUCCESS";
                         welcomeMessage(usernameIndex, usernames);
+                        break;
                     }
                     else {
-                        failTrys++;
                         System.out.println("Incorrect password, try again. You have " + --passwordTrys + " trys left");
                     }
                 } while (passwordTrys != 0);
@@ -33,12 +34,18 @@ public class LoginAnalyzer {
                     login = "FAILED";
                     System.out.println("Too many incorrect trys. Try again later");
                 }
-                loginLog(usernameIndex, login, usernames);
-                break;
+                loginCount = loginLog(usernameIndex, login, usernames, log, loginCount);
             }
             else {
                 System.out.println("Invalid username, try again");
             }  
+            System.out.print("Would you like to print login log? (y/n): ");
+            char printLog = input.next().charAt(0);
+            input.nextLine();
+            if (printLog == 'y') {
+                printLog(log);
+            }
+
         } while (welcomeLoop);
 
         input.close();
@@ -53,8 +60,7 @@ public class LoginAnalyzer {
         return -1;
     }
 
-    public static boolean passwordCheck(String loginPassword, int usernameIndex) {
-        String[] passwords = {"aeafhbk", "asdgs", "hdfssdh", "vsdfsd"};
+    public static boolean passwordCheck(String loginPassword, int usernameIndex, String[] passwords) {
         return loginPassword.equals(passwords[usernameIndex]);
     }
 
@@ -62,10 +68,17 @@ public class LoginAnalyzer {
         System.out.println("Hello " + usernames[usernameIndex]);
     }
 
-    public static void loginLog(int usernameIndex, String login, String[] usernames) {
-        String[] log = new String[10];
-        int loginCount = 0;
+    public static int loginLog(int usernameIndex, String login, String[] usernames, String[] log, int loginCount) {
         log[loginCount] = "LOGIN:" + usernames[usernameIndex] + ":" + login;
         loginCount++;
+        return loginCount;
+    }
+
+    public static void printLog(String[] log) {
+        for (int i = 0; i < log.length; i++) {
+            if (log[i] != null) {
+                System.out.println(log[i]);
+            }
+        }
     }
 }
